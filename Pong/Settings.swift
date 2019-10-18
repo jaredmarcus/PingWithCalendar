18c99dd7e5ec972de957425673df7e3ad0203f79
Robert F. Popeleski <therfp97@gmail.com>
2019-10-17 23:19:29 -0400

    Clock functionality works inside ball

---
+++ Pong/Settings.swift
@@ -0,0 +1,32 @@
+//
+//  Settings.swift
+//  Pong
+//
+//  Created by Robert Popeleski on 10/17/19.
+//  Copyright © 2019 Jared Macias. All rights reserved.
+//
+
+import ScreenSaver
+
+class Settings {
+    let defaults = Settings.screenSaverDefaults()
+    func dateFormat() -> String {
+        if defaults.bool(forKey: "isTwelveHour") {
+            return "h:mm:ss"
+        } else {
+            return "HH:mm:ss"
+        }
+    }
+    
+    private static func screenSaverDefaults() -> ScreenSaverDefaults {
+        guard let bundleId = Bundle(for: Settings.self).bundleIdentifier else {
+            fatalError("Could not find a bundle identifier")
+        }
+
+        guard let defaults = ScreenSaverDefaults(forModuleWithName: bundleId) else {
+            fatalError("Could not create ScreenSaverDefaults instance")
+        }
+
+        return defaults
+    }
+}
